0ea1b4a6de4127dda2a798fb65110392e7f1d2cb
Maurizio De Magnis <root@olisti.co>
2016-11-26 17:55:06 +0100

    sample

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
 gem "trailblazer-operation", github: "trailblazer/operation"
-gem "trailblazer", github: "trailblazer/trailblazer"
+# gem "trailblazer", github: "trailblazer/trailblazer"
+gem "activerecord"
+gem "sqlite3"
--- playground.rb
+++ playground.rb
@@ -1,2 +1,37 @@
 require "trailblazer/operation"
+require 'active_record'
 
+# class Create < Trailblazer::Operation
+#   self.> :hello!
+#   self.> :ciao!
+#
+#   def hello!(*)
+#     p "hello"
+#   end
+#
+#   def ciao!(*)
+#     p "ciao"
+#   end
+# end
+#
+# Create.()
+
+
+# Change the following to reflect your database settings
+ActiveRecord::Base.establish_connection(
+  adapter:  'sqlite3',
+  database: 'db.sqlite3',
+)
+
+ActiveRecord::Schema.define do
+  create_table :posts, force: true do |t|
+    t.string :title
+  end
+end
+
+class Post < ActiveRecord::Base
+end
+
+post = Post.create(title: "Trailblazer!")
+p post.title
+p Post.count
